fef0d36698305844e09b31e3c108786c3a8ce5a2
Vityoka <weetjoue@gmail.com>
2025-08-24 17:17:00 +0200

    add some comments

--- tests/test_csv_handler.py
+++ tests/test_csv_handler.py
@@ -1,8 +1,11 @@
 
-from src.csv_handler.csv_handler import CsvHandler
+from csv_handler.csv_handler import CsvHandler
 
 def test_csv_handler_initialization():
+    # ARRANGE
     path = "data/data.csv"
+    # ACT
     csv_handler = CsvHandler(path)
+    # ASSERT
     assert csv_handler.csv_path == path
     assert not csv_handler.csv_df.empty
